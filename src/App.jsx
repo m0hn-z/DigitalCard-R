033edfd1b17f43326b3fb35f709b4d9d87259e3b
m0hn-z <167054270+m0hn-z@users.noreply.github.com>
2024-06-28 09:21:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Info from './components/Info'
@@ -7,7 +7,6 @@ import Footer from './components/Footer'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='container'>
